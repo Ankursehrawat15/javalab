4d4a8aa27bd9694c25c5de07a35942faa7988ccb
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-03-19 12:15:26 -0700

    Create array.java

---
+++ array.java
@@ -0,0 +1,18 @@
+
+int intArray[]; 
+or int[] intArray; 
+
+byte byteArray[];
+short shortsArray[];
+boolean booleanArray[];
+long longArray[];
+float floatArray[];
+double doubleArray[];
+char charArray[];
+
+
+MyClass myClassArray[]; 
+
+Object[]  ao,     
+Collection[] ca;  
+                 
