080bdb8fc4f350937831fd8953076f6802eed0a5
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-03-21 23:30:01 -0700

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,12 @@
+class ArrayExample {
+   public static void main(String[] args) {
+      
+      int[] age = new int[5];
+      
+      System.out.println(age[0]);
+      System.out.println(age[1]);
+      System.out.println(age[2]);
+      System.out.println(age[3]);
+      System.out.println(age[4]);
+   }
+}
