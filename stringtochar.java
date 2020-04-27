3dc65457b7f63e83722a8edff16b99d4e69ca2d1
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-04-27 09:16:52 -0700

    Create stringtochar.java

---
+++ stringtochar.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class StringChar {
+
+    public static void main(String[] args) {
+        String st = "This is great";
+
+        char[] chars = st.toCharArray();
+        System.out.println(Arrays.toString(chars));
+    }
+}
