6926780ceb7f0c964f71bb6c02f663cc698d2087
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-07 00:04:44 +0530

    Update HashMap.java

--- java/HashMap.java
+++ java/HashMap.java
@@ -15,3 +15,8 @@ String key = "Ankur";
 if (map.containsKey(key)) 
 {
 Integer a = map.get(key); 
+System.out.println("value for key "+ key  +" is: "+a); 
+}
+int value = 20; 
+System.out.println();
+if (map.containsValue(value)) 
