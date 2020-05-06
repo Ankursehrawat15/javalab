fc9654e10d005649a63991399320ba811669aabe
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-07 00:05:19 +0530

    Update HashMap.java

--- java/HashMap.java
+++ java/HashMap.java
@@ -20,5 +20,14 @@ System.out.println("value for key "+ key  +" is: "+a);
 int value = 20; 
 System.out.println();
 if (map.containsValue(value)) 
+{Iterator it = map.entrySet().iterator();
+while(it.hasNext()){ 
+String str = it.next().toString();
+if(str.split("=")[1].contains(String.valueOf(value)))
 {
+System.out.println("key for value "+ value  +" is: "+str.split("=")[0]); 
+}
+}
+} 
+
 
