2d217e2a249c2e1955308a7d3c8603d098fa67ee
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-06 23:44:01 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,68 @@
+class sports
+{
+String name;
+int age;
+sports(String n, int a)
+{name=n; age=a;}
+void show()
+{
+System.out.println("\n");
+System.out.println("Player name : "+name);
+System.out.println("Age : "+age);
+}
+}
+class cricketer extends sports
+{
+String type;
+cricketer(String n, String t,int a)
+{
+super(n,a);
+type=t;
+}
+public void show()
+{
+super.show();
+System.out.println("Player plays : "+type);
+}
+}
+class football_player extends sports
+{
+String type;
+football_player(String n, String t,int a)
+{
+super(n,a);
+type=t;
+}
+public void show()
+{
+super.show();
+System.out.println("Player plays : "+type);
+}
+}
+class hockey_player extends sports
+{
+String type;
+hockey_player(String n, String t,int a)
+{
+super(n,a);
+type=t;
+}
+public void show()
+{
+super.show();
+System.out.println("Player plays : "+type);
+}
+}
+// main
+class Player
+{
+public static void main(String args[])
+{
+cricketer c1 = new cricketer("Viratkholi","Cricket",35);
+football_player c2 = new football_player("Messi","Football",40);
+hockey_player c3 = new hockey_player("Salah","Hockey",25);
+c1.show();
+c2.show();
+c3.show();
+}
+}
