e9a35cc42997a4bddcfcbee3838b49a965a9e205
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-07 00:02:58 +0530

    Update HashMap.java

--- java/HashMap.java
+++ java/HashMap.java
@@ -5,3 +5,9 @@ class NewHashMap
 {
 public static void main(String args[])
 {
+HashMap<String, Integer> map = new HashMap<>(); 
+map.put("Ankur", 20); 
+map.put("Aayush", 10); 
+map.put("Brock", 25);
+map.put("Kriti", 40);
+System.out.println();
