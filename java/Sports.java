562401ff549b956c2997de78c8ab37d0fddc5a36
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-07 23:02:21 +0530

    Update Sports.java

--- java/Sports.java
+++ java/Sports.java
@@ -13,3 +13,41 @@ System.out.println("Player name : "+name);
 System.out.println("Age : "+age);
 }
 }
+  class cricketer extends sports
+{
+String type;
+cricketer(String n, String t,int a)
+{
+super(n,a);
+type=t;
+}
+public void show()
+{
+super.show();
+System.out.println("Player plays : "+type);
+}
+}
+class football_player extends sports
+{
+String type;
+football_player(String n, String t,int a)
+{
+super(n,a);
+type=t;
+}
+public void show()
+{
+super.show();
+System.out.println("Player plays : "+type);
+}
+}
+class hockey_player extends sports
+{
+String type;
+hockey_player(String n, String t,int a)
+{
+super(n,a);
+type=t;
+}
+
+}
