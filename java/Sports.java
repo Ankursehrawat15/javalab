114d9b662109b4471ce896e963768bd48bd26673
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-07 23:01:56 +0530

    Create Sports.java

---
+++ java/Sports.java
@@ -0,0 +1,15 @@
+class sports{
+
+public static void main(String[] args)
+{
+String name;
+int age;
+sports(String n, int a)
+{name=n; age=a;}
+void show()
+{
+System.out.println("\n");
+System.out.println("Player name : "+name);
+System.out.println("Age : "+age);
+}
+}
