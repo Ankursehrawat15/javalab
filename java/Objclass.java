0fee9817e5c0c06309ce4c91760f5e549b11d77f
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-08 21:33:00 +0530

    Update Objclass.java

--- java/Objclass.java
+++ java/Objclass.java
@@ -7,3 +7,8 @@ ObjClass obj1 = new ObjClass();
 
 if(obj1.x < obj1.y)
 System.out.println(obj1.x + " is lesser than" + obj1.y);
+
+else 
+System.out.println(obj1.x + "is greater than" + obj1.y);
+
+}}
