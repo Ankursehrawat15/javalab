4d0e225e57e8d0e04cdcbf4d46e75ea4bba625fd
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-09 21:51:35 +0530

    Update SumOfInt.java

--- java/SumOfInt.java
+++ java/SumOfInt.java
@@ -6,4 +6,10 @@ int sum=0;
 System.out.println("All integers which are greater than 40 and less than 250 that are divisible by 5 are :-");
 for (x=40;x<=250;x++)
 {
-
+if (x%5==0){
+System.out.println(" "+ x);
+sum = sum+x;
+}
+}
+System.out.println("And there sum is "+ sum);
+}}
