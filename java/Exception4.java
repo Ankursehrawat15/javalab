c565489a281e43470d0c51e4677f23ea8adffb3b
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-09 23:01:26 +0530

    Create Exception4.java

---
+++ java/Exception4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class direction extends Exception{
+direction(){
+super("collision");
+}
+}
+public class Exception4{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the direction same or opposite");
+String a=sc.nextLine();
+try
+{
+
