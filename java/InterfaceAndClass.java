a454613859e5e6142040ab717760a5a61b85dc64
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-09 21:57:55 +0530

    Update InterfaceAndClass.java

--- java/InterfaceAndClass.java
+++ java/InterfaceAndClass.java
@@ -6,4 +6,16 @@ void meth2();
 interface B extends A {
 void meth2();  //Overriding the method in A ??
 }
+class MyClass implements B {
+public void meth1() {
+System.out.println("Implement meth1().");
+}
+public void meth2() 
+{
+System.out.println("Implement meth2().");
+}
+public void meth3() {
+System.out.println("Implement meth3().");
+}
+}
 
