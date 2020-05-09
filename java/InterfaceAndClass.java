0e1551304461ffabbb287dfa171421b475fb805c
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-09 21:59:13 +0530

    Update InterfaceAndClass.java

--- java/InterfaceAndClass.java
+++ java/InterfaceAndClass.java
@@ -10,3 +10,18 @@ class MyClass implements B {
 public void meth1() {
 System.out.println("Implement meth1().");
 }
+public void meth2() 
+{
+System.out.println("Implement meth2().");
+}
+public void meth3() {
+System.out.println("Implement meth3().");
+}
+}
+class InterfaceAndClass {
+public static void main(String arg[]) {
+B ob = new MyClass(); // Interface refrence variable 
+ob.meth1();
+ob.meth2(); 
+}
+}
