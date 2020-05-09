81dd3ff98642f29a3acdb1a234123afb8dfac006
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-09 22:58:17 +0530

    Update Exception2.java

--- java/Exception2.java
+++ java/Exception2.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
 class demoexception {
 int num1, num2;
+void dividefunction() {
+System.out.println("Enter two numbers: ");
+Scanner in = new Scanner(System.in);
+num1 = in.nextInt();
+num2 = in.nextInt();
+try { // here it will throw exception
 
