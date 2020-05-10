9b126043328cfb8cd7963bacf98612e42c8b0238
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-10 21:37:42 +0530

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -14,4 +14,19 @@ public int square()
 return (b*b);
 }
 }
-
+class ToTestInt
+{
+public int return_ans(int x)
+{
+arithmetic a=new arithmetic(x);
+return a.square();
+}
+}
+class Interface
+{
+public static void main(String [] args)
+{
+ToTestInt x= new ToTestInt();
+System.out.println("\nThe square of 64 is "+x.return_ans(64));
+}
+}
