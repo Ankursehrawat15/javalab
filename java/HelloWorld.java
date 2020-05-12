ed77646ce118b5684e7e5090cf9dabba72b770d2
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-12 14:14:14 +0530

    Update HelloWorld.java

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 public static void main (String[] args)
 {
-system.out.println("Hello World");
+system.out.println("Hello World ");
+  system.out.println("this side java ");
 }
 }
