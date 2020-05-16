6ed5b74c7803fa40393a841e5c714ab1708619c5
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-16 13:51:46 +0530

    Update multipymatrix.java

--- multipymatrix.java
+++ multipymatrix.java
@@ -12,5 +12,23 @@ class MatrixMultiplication
     n = in.nextInt();
  
     int first[][] = new int[m][n];
+System.out.println("Enter elements of first matrix");
+ 
+    for (c = 0; c < m; c++)
+      for (d = 0; d < n; d++)
+        first[c][d] = in.nextInt();
+ 
+    System.out.println("Enter the number of rows and columns of second matrix");
+    p = in.nextInt();
+    q = in.nextInt();
+ 
+    if (n != p)
+      System.out.println("The matrices can't be multiplied with each other.");
+    else
+    {
+      int second[][] = new int[p][q];
+      int multiply[][] = new int[m][q];
+ 
+      
  
     
