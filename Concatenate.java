bf3823182d6cfd3a3ef4cc30bfd92caa11dcbbbb
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-16 13:48:19 +0530

    Update Concatenate.java

--- Concatenate.java
+++ Concatenate.java
@@ -1,5 +1,12 @@
 import java.util.Arrays;
 
 public class Concat {
+public static void main(String[] args) {
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 5, 6};
+
+        int length = array1.length + array2.length;
+
+       
 
     
