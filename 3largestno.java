a52029eebf8db6b35b9709c96a5ee3783ec5d8d5
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-16 13:43:58 +0530

    Update 3largestno.java

--- 3largestno.java
+++ 3largestno.java
@@ -7,7 +7,7 @@ int element=list.get(total-3);
 return element;  
 }  
 public static void main(String args[]){ 
-Integer a[]={1,2,5,7,3,8};  
+Integer a[]={1,2,5,7,6,8};  
 Integer b[]={44,33,99,77,33,66,22};  
 System.out.println("Third Largest: "+getThirdLargest(a,6));  
 System.out.println("Third Largest: "+getThirdLargest(b,7));  
